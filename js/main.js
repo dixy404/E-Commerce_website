d473e83a4483ee2ed71039d94eb2b6f57211ee8a
unknown <dixy@blic.net>
2020-10-08 00:35:36 +0200

    ...

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //MEGA MENU
-function myFunction(event) {
+/* function myFunction(event) {
   event.stopPropagation();
   document.getElementById("manDropdown").classList.toggle("show");
 }
@@ -29,4 +29,62 @@ function megaFunction() {
   } else {
     x.className = "nav-content";
   }
+} */
+
+//MEGA MENU
+const btnMan = document.querySelector("#btnMan");
+const btnWoman = document.querySelector("#btnWoman");
+const btnChild = document.querySelector("#btnChild");
+const manMenu = document.querySelector("#manDropdown");
+const womanMenu = document.querySelector("#womanDropdown");
+const childMenu = document.querySelector("#childDropdown");
+
+const icon = document.querySelector("#hamburgerIcon");
+const menu = document.querySelector("#mega-menu");
+
+//If the user clicks one dropdown close all others
+btnMan.addEventListener("click", manDropdown);
+
+function manDropdown() {
+  manMenu.classList.toggle("show");
+  womanMenu.classList.remove("show");
+  childMenu.classList.remove("show");
 }
+manDropdown();
+
+btnWoman.addEventListener("click", womanDropdown);
+
+function womanDropdown() {
+  womanMenu.classList.toggle("show");
+  manMenu.classList.remove("show");
+  childMenu.classList.remove("show");
+}
+womanDropdown();
+
+btnChild.addEventListener("click", childDropdown);
+
+function childDropdown() {
+  childMenu.classList.toggle("show");
+  manMenu.classList.remove("show");
+  womanMenu.classList.remove("show");
+}
+childDropdown();
+
+// Close the dropdown menu if the user clicks outside of it
+/* window.onclick = function (event) {
+  manMenu.classList.remove("show");
+  womanMenu.classList.remove("show");
+  childMenu.classList.remove("show");
+}; */
+
+//Responsive mega menu
+icon.addEventListener("click", responsiveMenu);
+
+function responsiveMenu() {
+  if (menu.className === "nav-content") {
+    menu.className += " responsive";
+  } else {
+    menu.className = "nav-content";
+  }
+}
+responsiveMenu();
