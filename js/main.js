043a6bf8627e3413f8b251857c52f5db70371c12
unknown <dixy@blic.net>
2020-10-31 21:02:32 +0100

    updated mega menu

--- js/main.js
+++ js/main.js
@@ -1,81 +1,41 @@
 //MEGA MENU
-/* function myFunction(event) {
-  event.stopPropagation();
-  document.getElementById("manDropdown").classList.toggle("show");
-}
-
-function myFunction2(event) {
-  event.stopPropagation();
-  document.getElementById("womanDropdown").classList.toggle("show");
-}
-
-function myFunction3(event) {
-  event.stopPropagation();
-  document.getElementById("childDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  document.getElementById("manDropdown").classList.remove("show");
-  document.getElementById("womanDropdown").classList.remove("show");
-  document.getElementById("childDropdown").classList.remove("show");
-};
-
-//Responsive mega menu
-function megaFunction() {
-  var x = document.getElementById("mega-menu");
-  if (x.className === "nav-content") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-content";
-  }
-} */
-
-//MEGA MENU
-const btnMan = document.querySelector("#btnMan");
-const btnWoman = document.querySelector("#btnWoman");
-const btnChild = document.querySelector("#btnChild");
-const manMenu = document.querySelector("#manDropdown");
-const womanMenu = document.querySelector("#womanDropdown");
-const childMenu = document.querySelector("#childDropdown");
-
 const icon = document.querySelector("#hamburgerIcon");
 const menu = document.querySelector("#mega-menu");
 
-//If the user clicks one dropdown close all others
-btnMan.addEventListener("click", manDropdown);
+(function () {
+  var dropBtns = document.querySelectorAll(".dropdown");
 
-function manDropdown() {
-  manMenu.classList.toggle("show");
-  womanMenu.classList.remove("show");
-  childMenu.classList.remove("show");
-}
-manDropdown();
+  function closeOpenItems() {
+    openMenus = document.querySelectorAll(".dropdown-content");
+    openMenus.forEach(function (menus) {
+      menus.classList.remove("show");
+    });
+  }
 
-btnWoman.addEventListener("click", womanDropdown);
+  dropBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      var dropContent = btn.querySelector(".dropdown-content"),
+        shouldOpen = !dropContent.classList.contains("show");
+      e.preventDefault();
 
-function womanDropdown() {
-  womanMenu.classList.toggle("show");
-  manMenu.classList.remove("show");
-  childMenu.classList.remove("show");
-}
-womanDropdown();
+      //Close all open items
+      closeOpenItems();
+      // Check if the clicked item should be opened
+      if (shouldOpen) {
+        // Open the clicked item
+        dropContent.classList.add("show");
+      }
+      e.stopPropagation();
+    });
+  });
 
-btnChild.addEventListener("click", childDropdown);
-
-function childDropdown() {
-  childMenu.classList.toggle("show");
-  manMenu.classList.remove("show");
-  womanMenu.classList.remove("show");
-}
-childDropdown();
-
-// Close the dropdown menu if the user clicks outside of it
-/* window.onclick = function (event) {
-  manMenu.classList.remove("show");
-  womanMenu.classList.remove("show");
-  childMenu.classList.remove("show");
-}; */
+  // Close the dropdown if the user clicks outside of it
+  window.addEventListener("click", function (event) {
+    if (event.target != dropBtns) {
+      closeOpenItems();
+    }
+  });
+})();
 
 //Responsive mega menu
 icon.addEventListener("click", responsiveMenu);
